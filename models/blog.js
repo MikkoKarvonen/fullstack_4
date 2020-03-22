5db1d4076a32faf619f774a8cd73ca574e885168
Mikko <mkarvonen34@gmail.com>
2020-03-22 18:38:29 +0200

    Tehtävät 4.1 ja 4.2

---
+++ models/blog.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
